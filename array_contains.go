d63395a5695d3ef289c8893d588441d296d514d1
Kareem Hepburn <kareem@hepburn.info>
2023-07-09 09:27:57 -0500

    feat: add generic function to find an element in a slice

--- array_contains.go
+++ array_contains.go
@@ -1,9 +1,29 @@
 package gobag
 
-// ArrayContainsStr ...
-func ArrayContainsStr(strSlice []string, searchString string) bool {
-	for _, value := range strSlice {
-		if value == searchString {
+// ArrayContainsStr checks if a given target string is present in the source
+// string slice. It iterates through the source slice and compares each element
+// with the target string. The function returns true if the target string is
+// found in the source slice, and false otherwise.
+//
+// Deprecated: Since Go 1.18, it is recommended to use SliceContains for
+// checking element presence in slices.
+func ArrayContainsStr(source []string, target string) bool {
+	for _, value := range source {
+		if value == target {
+			return true
+		}
+	}
+	return false
+}
+
+// SliceContains checks if a given target element is present in the source
+// slice. It iterates through the source slice and compares each element with
+// the target element. The function returns true if the target element is found
+// in the source slice, and false otherwise. The elements must be of a
+// comparable type, denoted by the type parameter T.
+func SliceContains[T comparable](source []T, target T) bool {
+	for i := range source {
+		if source[i] == target {
 			return true
 		}
 	}
--- array_contains_test.go
+++ array_contains_test.go
@@ -3,15 +3,35 @@ package gobag
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestArrayContainsStr(t *testing.T) {
 	array := []string{"hello", "there", "manbearpig"}
 
 	b := ArrayContainsStr(array, "hello")
-	assert.True(t, b)
+	require.True(t, b)
 
 	b = ArrayContainsStr(array, "momo")
-	assert.False(t, b)
+	require.False(t, b)
+}
+
+func TestCollectionContains(t *testing.T) {
+	t.Run("with strings", func(t *testing.T) {
+		collection := []string{"hello", "there", "manbearpig"}
+		result := SliceContains[string](collection, "hello")
+		require.True(t, result)
+
+		result = SliceContains[string](collection, "momo")
+		require.False(t, result)
+	})
+
+	t.Run("with ints", func(t *testing.T) {
+		collection := []int{1, 2, 3, 4, 5, 6}
+		result := SliceContains[int](collection, 2)
+		require.True(t, result)
+
+		result = SliceContains[int](collection, 22)
+		require.False(t, result)
+	})
 }
